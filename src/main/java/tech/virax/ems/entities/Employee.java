4be98d7e1cfa4e0b1e6f91d70ab227011218649b
Virjai <vinayjais98@gmail.com>
2026-08-16 19:39:02 +0530

    Initial project set up and table creation

---
+++ src/main/java/tech/virax/ems/entities/Employee.java
@@ -0,0 +1,39 @@
+package tech.virax.ems.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employee")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @Column(name = "e_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "e_name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "e_age", nullable = false)
+    private int age;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "e_salary", nullable = false)
+    private double salary;
+
+    @Column(name = "e_designation", nullable = false)
+    private String designation;
+
+}
---
+++ src/test/java/tech/virax/ems/EmsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.virax.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
